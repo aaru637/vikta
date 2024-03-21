637b286d5f14a0e72c0db85e8bf9bc0d5dad3b83
DK <dhandapanisakthi123@gmail.com>
2024-03-22 02:16:48 +0530

    abcd

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import "./App.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/about/About";
+import NavBar from "./components/navbar/NavBar";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<About />} />
+        <Route path="/" element={
+          <div className={`home bg-cover`}>
+            <NavBar />
+            <Home />
+          </div>} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
