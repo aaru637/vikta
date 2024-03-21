575ff08477d2317a4f63ecc58e7c7241b3af38ac
DK <dhandapanisakthi123@gmail.com>
2024-03-21 21:57:47 +0530

    File update

---
+++ src/utils/HomeUtils.jsx
@@ -0,0 +1,39 @@
+// Images
+const gcelogo = '../assets/images/gcelogo.jpeg';
+const assoclogo = '../assets/images/associationlogo.jpg';
+const homebg = '../assets/images/home_bg.jpg';
+
+// navbar
+const navbarTitles = [
+    {
+        id: 'home',
+        value: 'Home'
+    },
+    {
+        id: 'aboutus',
+        value: 'About Us'
+    },
+    {
+        id: 'events',
+        value: 'Events'
+    },
+    {
+        id: 'contactus',
+        value: 'Contact Us'
+    },
+];
+
+// texts
+const college1 = 'Government College of Engineering';
+const college2 = 'Dharmapuri - 636704';
+const department = 'Department of CSE';
+const association1 = 'Association of Computer Science Engineers';
+const association2 = 'Proudly Presents';
+const vikta = 'VIKTA.2-24';
+const sympo = 'National Level Technical Symposium';
+const sympoDate = 'April 8th 2024';
+const secretary = ['General Secretary', 'Join Secretary'];
+const secretaryNames = ['Varunkumar S', 'Sangeetha M'];
+
+export { gcelogo, assoclogo, college1, college2, department, association1, association2, vikta, sympo, sympoDate, secretary, secretaryNames, navbarTitles }
+export {homebg}
